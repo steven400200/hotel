0fde384be8be7c4fd367d034ab77dfc9f65efbd0
steven123 <134459485+steven400200@users.noreply.github.com>
2024-08-25 13:49:38 -0500

    creacion de clases

---
+++ src/main/java/co/edu/uniquindio/poo/hotel/controller/ControllerReserva.java
@@ -0,0 +1,25 @@
+package co.edu.uniquindio.poo.hotel.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerReserva implements Initializable {
+    @FXML
+    private TextField textId;
+
+    @FXML
+    private Button btnReservar;
+
+    @FXML
+    private TextField textNombre;
+
+    @FXML
+    void guardarReserva(ActionEvent event){}
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
